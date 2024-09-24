e1c794baeadb649a0b7357e57c3b43c1cfae5033
채다희 <cdh010126r@gmail.com>
2024-09-24 11:20:03 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    const num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    
+    for(let i=0; i<num.length; i++){
+        s = s.split(num[i]).join(i);
+    }
+    return Number(s);
+}
