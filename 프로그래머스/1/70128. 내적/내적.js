381ebfeba82d00e4fd45c905d579645ab0aab1af
채다희 <cdh010126r@gmail.com>
2024-10-01 11:09:11 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,8 +1,7 @@
 function solution(a, b) {
     var ans = 0;
     for(let i=0; i<a.length; i++){
-        let temp = a[i]*b[i]
-        ans += temp
+        ans += a[i]*b[i]
     }
     return ans;
 }
