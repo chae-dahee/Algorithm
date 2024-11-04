e316833a1b8c6bc69471b33944966713a9b67c45
채다희 <cdh010126r@gmail.com>
2024-11-04 14:24:34 +0900

    [level 2] Title: 귤 고르기, Time: 103.15 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,23 @@
+//귤 개수 k
+//서로 다른 종류 최소
+//배열에서 k 개 만큼 뽑아낼것
+//크기순 나열, 중복인 개수 파악해서
+//종류 ans
+function solution(k, tangerine) {
+    var ans = 0;
+    let dict = {}
+    
+    tangerine.map((val)=>{
+        dict[val] = (dict[val]||0)+1
+    });
+    
+    let arr = Object.values(dict).sort((a,b)=>b-a);
+    
+    for(const x of arr){
+        ans++;
+        if(k>x) k-=x;
+        else break;
+    }
+        
+    return ans;
+}
