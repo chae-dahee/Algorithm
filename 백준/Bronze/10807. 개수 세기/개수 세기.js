eb8d501f0fc7c13256c6f30c2320c75569129e0e
채다희 <cdh010126r@gmail.com>
2025-04-21 11:43:17 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.js
+++ 백준/Bronze/10807. 개수 세기/개수 세기.js
@@ -3,11 +3,8 @@ const filePath =
   process.platform === "linux" ? "dev/stdin" : "1차원배열/10870.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const N = input[0];
 const arr = input[1].split(" ");
 const findNum = input[2];
-let ans = [];
-
-ans = arr.filter((v) => v === findNum);
+const ans = arr.filter((v) => v === findNum);
 
 console.log(ans.length);
