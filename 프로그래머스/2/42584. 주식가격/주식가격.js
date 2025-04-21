3a2b41ac0a3c064f6dcd5c108a35fe01c540e3a0
채다희 <cdh010126r@gmail.com>
2025-04-21 10:50:39 +0900

    [level 2] Title: 주식가격, Time: 31.34 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,10 +1,23 @@
-function solution(prices) {    
-    return prices.map((e,i)=>{
-        for(let j=i+1; j<prices.length; j++){
-            if(prices[j]<e) return j-i
-        }
-        return prices.length -i -1
-    })
-}
+// function solution(prices) {    
+//     return prices.map((e,i)=>{
+//         for(let j=i+1; j<prices.length; j++){
+//             if(prices[j]<e) return j-i
+//         }
+//         return prices.length -i -1
+//     })
+// }
 
 //배열 for문 순회하면서 현재 나 val 값 이후 배열인덱스 작은 val 의 idx 찾음. 작은 val idx - 나의 idx -> 스택에 넣음
+
+function solution(prices) {    
+    let ans = []
+    for(let i=0; i<prices.length; i++){
+        let cnt = 0
+        for(let j=i+1; j<prices.length; j++){
+            cnt++
+            if(prices[i]>prices[j]) break
+        }
+        ans.push(cnt)
+    }
+    return ans
+}
