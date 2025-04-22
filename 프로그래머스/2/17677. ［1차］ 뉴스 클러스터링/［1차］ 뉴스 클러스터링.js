34bc31a7216f9f456129baff610aba1d0a5ee182
채다희 <cdh010126r@gmail.com>
2025-04-22 10:59:57 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 4.58 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -0,0 +1,38 @@
+function solution(str1, str2) {
+    var ans = 0;
+    const arr1 = makeStr(str1)
+    const arr2 = makeStr(str2)
+    if(arr1.length ===0 && arr2.length ===0) return 65536
+    console.log(arr1, arr2)
+    
+    let intersection = []
+    let union = []
+    
+    for(let i=0; i<arr1.length; i++){
+        if(arr2.includes(arr1[i])){
+            const idx = arr2.indexOf(arr1[i])
+            arr2.splice(idx,1)
+            intersection.push(arr1[i])
+        }else{
+            union.push(arr1[i])
+        }
+    }
+    arr2.forEach(e=>union.push(e))
+    
+    return Math.floor((intersection.length/(intersection.length + union.length))*65536)
+    
+    return ans;
+}
+
+function makeStr(str){
+    const arr=[]
+    const regex = /[\W0-9_]/
+    str.split('')
+    
+    for(let i = 1 ; i < str.length; i ++) {
+        const elem = str[i-1] + str[i];
+        if(regex.test(elem)) continue;
+        arr.push(elem.toLowerCase());
+    }
+    return arr
+}
