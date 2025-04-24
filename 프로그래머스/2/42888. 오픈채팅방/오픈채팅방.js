6ce18df2ecaa79b5e775f36c92d962b5657c18bc
채다희 <cdh010126r@gmail.com>
2025-04-24 11:32:25 +0900

    [level 2] Title: 오픈채팅방, Time: 341.08 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,21 +1,20 @@
 function solution(record) {
     var ans = [];
-    const map = new Map()
+    const uids = new Map()
     
     record.forEach(e=>{
-        const state = e.split(' ')
-        if(state[0]==='Enter'||state[0]==='Change'){
-           map.set(state[1], state[2])
+        let [command, uid, nick] = e.split(' ')
+        if(command==='Enter'||command==='Change'){
+           uids.set(uid, nick)
        }
     })
     record.forEach(e=>{
-        const state = e.split(' ')
-        let name = map.get(state[1])
-        if(state[0]==='Enter'){
-            ans.push(`${name}님이 들어왔습니다.`)
+        const [command, uid] = e.split(' ')
+        if(command==='Enter'){
+            ans.push(`${uids.get(uid)}님이 들어왔습니다.`)
         }
-        else if(state[0]==='Leave'){
-            ans.push(`${name}님이 나갔습니다.`)
+        if(command==='Leave'){
+            ans.push(`${uids.get(uid)}님이 나갔습니다.`)
         }
     })
     
