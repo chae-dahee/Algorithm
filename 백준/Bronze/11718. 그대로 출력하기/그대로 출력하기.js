e0dea8c810e32a36116c80d3b8d3e09cd1ee8118
채다희 <cdh010126r@gmail.com>
2025-04-24 12:05:47 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
 const filePath =
   process.platform === "linux" ? "dev/stdin" : "문자열/11718.txt";
-const input = fs.readFileSync(filePath).toString().trim();
-console.log(input);
+console.log(fs.readFileSync(filePath).toString().trim());
