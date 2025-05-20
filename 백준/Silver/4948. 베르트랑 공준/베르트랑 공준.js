b697791b6e0d65338d8ba4f424fb2df8f01fdb8f
채다희 <cdh010126r@gmail.com>
2025-05-20 11:02:08 +0900

    [Silver II] Title: 베르트랑 공준, Time: 176 ms, Memory: 12068 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.js
@@ -0,0 +1,19 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "15_약수배수소수2/4948.txt";
+const fs = require("fs");
+const input = fs.readFileSync(filePath).toString().trim().split("\n").slice(0, -1).map((v) => +v);
+const prime = Array(123456 * 2 + 1).fill(true);
+for (let i = 2; i <= 123456 * 2 + 1; i++) {
+  if (!prime[i]) continue;
+  for (let j = i * 2; j <= 123456 * 2 + 1; j += i) {
+    prime[j] = false;
+  }
+}
+const output = [];
+for (const n of input) {
+  let result = 0;
+  for (let i = n + 1; i <= n * 2; i++) {
+    if (prime[i]) result++;
+  }
+  output.push(result);
+}
+console.log(output.join("\n"));
