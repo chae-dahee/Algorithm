4923a1bcdaf935ac4bf260165777e80486520a8b
채다희 <cdh010126r@gmail.com>
2025-06-04 11:54:19 +0900

    [Silver III] Title: N과 M (1), Time: 1592 ms, Memory: 13804 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,19 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "20_백트래킹/15649.txt";
+const [N, M] = require("fs").readFileSync(filePath).toString().trim().split(" ").map(Number);
+const visited = new Array(N + 1).fill(false);
+const arr = [];
+const dfs = (depth) => {
+  if (depth === M) {
+    console.log(arr.join(" "));
+    return;
+  }
+  for (let i = 1; i <= N; i++) {
+    if (!visited[i]) {
+      visited[i] = true;
+      arr[depth] = i;
+      dfs(depth + 1);
+      visited[i] = false;
+    }
+  }
+};
+dfs(0);
