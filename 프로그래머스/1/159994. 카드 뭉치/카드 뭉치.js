0e4ba690527660d8774e3f7dd67574e9c85b568f
채다희 <cdh010126r@gmail.com>
2025-06-25 11:44:41 +0900

    [level 1] Title: 카드 뭉치, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -3,8 +3,8 @@ function solution(cards1, cards2, goal) {
     let idx2 = 0;
     
     for(let i=0; i<goal.length; i++){
-        if(idx1 < cards1.length && cards1[idx1] === goal[i]) idx1++;
-        else if(idx2 < cards2.length && cards2[idx2] === goal[i]) idx2++;
+        if(cards1[idx1] === goal[i]) idx1++;
+        else if(cards2[idx2] === goal[i]) idx2++;
         else return 'No';
     }
     return 'Yes';
