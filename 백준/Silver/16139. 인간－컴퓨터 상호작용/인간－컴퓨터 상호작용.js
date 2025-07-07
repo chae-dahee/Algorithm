fcb53067c5637d79b6d17112e9c93254b0569101
채다희 <cdh010126r@gmail.com>
2025-07-07 10:45:13 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 804 ms, Memory: 109564 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.js
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.js
@@ -1,21 +1,28 @@
 const filePath = process.platform === "linux" ? "dev/stdin" : "22_누적합/16139.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-const S = input.shift();
-const q = parseInt(input.shift());
+const S = input[0];
+const q = parseInt(input[1]);
+let result = [];
+
+const usedChars = new Set();
+for (let i = 2; i < 2 + q; i++) {
+  const alpha = input[i].split(" ")[0];
+  usedChars.add(alpha);
+}
 
 const prefixSum = {};
-for (let i = 0; i < 26; i++) {
-  const char = String.fromCharCode(97 + i);
+for (const char of usedChars) {
   prefixSum[char] = new Array(S.length + 1).fill(0);
-
-  for (let j = 0; j < S.length; j++) {
+    for (let j = 0; j < S.length; j++) {
     prefixSum[char][j + 1] = prefixSum[char][j] + (S[j] === char ? 1 : 0);
   }
 }
 
 for (let i = 0; i < q; i++) {
-  const [alpha, l, r] = input[i].split(" ");
-  const count = prefixSum[alpha][parseInt(r) + 1] - prefixSum[alpha][parseInt(l)];
-  console.log(count);
+  const [alpha, left, right] = input[i + 2].split(" ");
+  const count = prefixSum[alpha][parseInt(right) + 1] - prefixSum[alpha][parseInt(left)];
+  result.push(count);
 }
+
+console.log(result.join("\n"));
