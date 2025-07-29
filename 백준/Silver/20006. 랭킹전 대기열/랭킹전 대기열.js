63264f57587234922f6be2736df91784d54cfd4f
채다희 <cdh010126r@gmail.com>
2025-07-29 11:58:44 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 124 ms, Memory: 10992 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.js
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.js
@@ -1,30 +1,30 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const [p, m] = input[0].split(' ').map(Number);
-const players = input.slice(1).map(val => {
-    const [level, name] = val.split(' ');
-    return { level: parseInt(level), name };
+const filePath = process.platform === "linux" ? "/dev/stdin" : "찬호스쿨_0729/20006.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+const [p, m] = input[0].split(" ").map(Number);
+const players = input.slice(1).map((val) => {
+  const [level, name] = val.split(" ");
+  return { level: Number(level), name };
 });
-function matching(p, m, players) {
-    const rooms = [];
-    for (const player of players) {
-        let matched = false;
-        for (const room of rooms) {
-            if (room.length < m && Math.abs(room[0].level - player.level) <= 10) {
-                room.push(player);
-                matched = true;
-                break;
-            }
-        }
-        if (!matched) {
-            rooms.push([player]);
-        }
+
+const rooms = [];
+
+for (const player of players) {
+  let joined = false;
+  for (const room of rooms) {
+    if (room.length < m && Math.abs(player.level - room[0].level) <= 10) {
+      room.push(player);
+      joined = true;
+      break;
     }
-    for (const room of rooms) {
-        console.log(room.length === m ? 'Started!' : 'Waiting!');
-        room.sort((a, b) => a.name.localeCompare(b.name));
-        for (const player of room) {
-            console.log(`${player.level} ${player.name}`);
-        }
-    }
+  }
+
+  if (!joined) rooms.push([player]);
 }
-matching(p, m, players);
+
+for (const room of rooms) {
+  console.log(room.length >= m ? "Started!" : "Waiting!");
+
+  room.sort((a, b) => a.name.localeCompare(b.name));
+  room.forEach((player) => console.log(`${player.level} ${player.name}`));
+}
