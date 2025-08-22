bcb8e600041561e533a200dec9105c3027ac7926
채다희 <cdh010126r@gmail.com>
2025-08-22 15:34:28 +0900

    Time: 0 ms (100%), Space: 53.6 MB (75.39%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+const gcd = (a, b) =>{
+    if(b === 0) return a
+    return gcd(b, a % b)
+ }
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+ if(str1 + str2 !== str2 + str1) return ""
+
+ const gcdLength = gcd(str1.length, str2.length)
+ return str1.substring(0, gcdLength)
+};
