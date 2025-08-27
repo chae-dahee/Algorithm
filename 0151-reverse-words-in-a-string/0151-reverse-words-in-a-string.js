bed45438a034fa2b83b3b7edccedd94591b3da41
채다희 <cdh010126r@gmail.com>
2025-08-27 10:13:49 +0900

    Delete 0151-reverse-words-in-a-string directory

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} s
- * @return {string}
- */
-var reverseWords = function(s) {
-    let word = s.split(' ')
-    let ans = word.filter(Boolean)
-    return ans.reverse().join(' ')
-};
